dfcfc2e2c1e38355456829849f3e482a9524f1d1
Martin Hruska <hruskamartin25@gmail.com>
2014-04-12 13:47:33 +0200

    improved tests to be able detect overflow

--- projekt3/myTest.py
+++ projekt3/myTest.py
@@ -19,11 +19,11 @@ y = random.randint(pow(2,i),pow(2,i+1)-1)
 xb = "{0:b}".format(x)
 yb = "{0:b}".format(y)
 x = lambda x,y: (bin(int(x,2)+int(y,2))[2:])
-f.write(getBin(xb))
-#f.write(xb)
+#f.write(getBin(xb))
+f.write(xb)
 f.write('\n')
-f.write(getBin(yb))
-#f.write(yb)
+#f.write(getBin(yb))
+f.write(yb)
 op = open("operands",'w')
 op.write(xb)
 op.write('\n')
@@ -32,6 +32,7 @@ op.close()
 s = x(xb,yb)
 if s > len(xb):
     print s[1:]
+    print "overflow"
 else:
     print s
 f.close()
--- projekt3/parseOut.py
+++ projekt3/parseOut.py
@@ -3,12 +3,17 @@ import sys
 f = open(sys.argv[1],'r')
 
 r = {}
+o = False
 for l in f:
     if ':' in l:
         l = l.rstrip()
         p = l.split(':')
         r[int(p[0])] = p[1]
+    if "overflow" in l:
+        o = True
 res = ""
 for c in r.keys():
     res = r[c] + res
 print res
+if o:
+    print "overflow"
